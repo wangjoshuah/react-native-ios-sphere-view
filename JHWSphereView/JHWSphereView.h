f8f4d383e5bbbf82aba4950c4bcce94059a4725d
Joshua H. Wang <wangjoshuah@gmail.com>
2018-10-02 12:30:34 -0400

    Initial Commit

---
+++ JHWSphereView/JHWSphereView.h
@@ -0,0 +1,19 @@
+//
+//  JHWSphereView.h
+//  JHWSphereView
+//
+//  Created by Josh Wang on 10/2/18.
+//  Copyright © 2018 wangjoshuah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHWSphereView.
+FOUNDATION_EXPORT double JHWSphereViewVersionNumber;
+
+//! Project version string for JHWSphereView.
+FOUNDATION_EXPORT const unsigned char JHWSphereViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHWSphereView/PublicHeader.h>
+
+
